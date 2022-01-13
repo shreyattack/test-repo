a5e2fade7250407f4e8bfa1e4534478ff770a3d1
shreyattack <shreyadey598@gmail.com>
2022-01-13 11:08:56 +0530

    initial commit

---
+++ eclipse-workspace/loops/src/loops/MyNumber.java
@@ -0,0 +1,52 @@
+package loops;
+
+public class MyNumber {
+
+	private int number;
+
+	public MyNumber(int number) {
+		this.number = number;
+
+	}
+
+	public boolean isPrime() {
+		if (number < 2) {
+			return false;
+		}
+		for (int i = 2; i <= number - 1; i++) {
+			if (number % i == 0)
+				return false;
+
+		}
+		return true;
+	}
+
+	public int sumUptoN() {
+		int sum = 0;
+		for (int i = 1; i <= number; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+
+	public int sumOfDivisors() {
+		int sumOfDivisors = 0;
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
+				sumOfDivisors += i;
+			}
+		}
+		return sumOfDivisors;
+	}
+
+	public void printNumberTriangle() {
+		for (int i = 1; i <= number; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
